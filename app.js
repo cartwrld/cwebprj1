6f6e342b786d79cb7e9dd50c4ffed5d8a4708089
Carter Walsh <cartwalsh@gmail.com>
2023-10-27 12:34:39 -0600

    started integrating google sign in. changed the index to homepage, and made it so the first page the user sees is the login page.

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ const SQLite = require('better-sqlite3');
 const SQLiteStore = require('better-sqlite3-session-store')(session);
 
 const sessOptions = {
-  secret: 'gotta catch em all', // must be the same secret that cookieParser is using
-  name: 'session-id',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {httpOnly: false, maxAge: 1000 * 60 * 60},
-  unset: 'destroy',
-  store: new SQLiteStore({
-    client: new SQLite('poke.db', {verbose: console.log}),
-    expired: {clear: true, intervals: 1000 * 60 * 15},
-  }),
+    secret: 'gotta catch em all', // must be the same secret that cookieParser is using
+    name: 'session-id',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {httpOnly: false, maxAge: 1000 * 60 * 60},
+    unset: 'destroy',
+    store: new SQLiteStore({
+        client: new SQLite('poke.db', {verbose: console.log}),
+        expired: {clear: true, intervals: 1000 * 60 * 15},
+    }),
 };
 
 
-const indexRouter = require('./routes/index');
+const homeRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
 const pokeBuilderRouter = require('./routes/pokebuilder');
 const teamBuilderRouter = require('./routes/teambuilder');
@@ -52,30 +52,30 @@ app.use(express.static('public'));
 app.use(session(sessOptions));
 app.use(passport.initialize({userProperty: 'currentUser'}));
 
-app.use('/', indexRouter);
+app.use('/home', homeRouter);
 app.use('/users', usersRouter);
 app.use('/teambuilder', teamBuilderRouter); // This is what determines the path name
 app.use('/pokebuilder', pokeBuilderRouter); // This is what determines the path name
-app.use('/login', loginRouter); // This is what determines the path name
+app.use('/', loginRouter); // This is what determines the path name
 app.use('/recentactivity', recentActivityRouter); // This is what determines the path name
 
 app.use('/bw', express.static(__dirname + '/node_modules/bootswatch/dist'));
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
